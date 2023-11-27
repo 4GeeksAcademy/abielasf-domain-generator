cd6433efe16f09e579ac6f7e0b7352c26eb86e60
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2023-11-27 18:36:07 +0000

    JS working, modified styles.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "my", "your", "her"];
+  let adjective = ["cute", "ugly", "big", "little"];
+  let noun = ["apple", "car", "phone", "home"];
+  let domain = [".es", ".io", ".net", ".org", ".com"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adjective.length; x++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let y = 0; y < domain.length; y++) {
+          let list = document.createElement("li");
+          list.className = "text-dark";
+          list.innerText = `${pronoun[i]}${adjective[x]}${noun[j]}${domain[y]}`;
+          document.querySelector("#domains").appendChild(list);
+        }
+      }
+    }
+  }
 };
